cebe4a9843b4270861d5f15c0622b61506effb96
JordanGuevaraChalial <jaguevara11@espe.edu.ec>
2026-02-10 22:40:12 -0500

    Cambios en el user.controller para que se pueda cambiar la contraseña

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/User'); 
-
+const bcrypt = require('bcryptjs');
 exports.obtenerPerfil = async (req, res) => {
   try {
     const user = await User.findByPk(req.userId, {
@@ -18,12 +18,26 @@ exports.obtenerPerfil = async (req, res) => {
 
 exports.actualizarPerfil = async (req, res) => {
   try {
-    const { nombre, foto_perfil } = req.body;
+    const { nombre, foto_perfil, password, passwordold } = req.body;
 
     // Solo permitimos actualizar nombre y foto_perfil (por seguridad)
     const updates = {};
     if (nombre !== undefined) updates.nombre = nombre;
     if (foto_perfil !== undefined) updates.foto_perfil = foto_perfil;
+
+    if (password !== undefined && passwordold !== undefined) {
+      const user = await User.findByPk(req.userId);
+      if (!user) {
+        return res.status(404).json({ message: "Usuario no encontrado" });
+      }
+
+      const isPasswordValid = await bcrypt.compare(passwordold, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: "Contraseña antigua incorrecta" });
+      }
+
+      updates.password = await bcrypt.hash(password, 12);
+    }
 
     if (Object.keys(updates).length === 0) {
       return res.status(400).json({ message: "No se proporcionaron campos para actualizar" });
